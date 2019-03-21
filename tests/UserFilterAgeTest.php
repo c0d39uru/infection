cd2373a330558e0a5271c85b5a322d12f34ebb0c
Mohammad Khaledi <mohammad.khaledi@acquia.com>
2019-03-21 17:11:50 -0400

    Fix unit test to cover edge case(s) and hence kill the mutants

--- tests/UserFilterAgeTest.php
+++ tests/UserFilterAgeTest.php
@@ -7,13 +7,30 @@ class UserFilterAgeTest extends TestCase {
 
   /**
    * @covers \InfectionDemo\UserFilterAge::filter
+   * @dataProvider usersProvider
+   *
+   * @param array $users
+   * @param int $expectedCount
    */
-  public function test_it_filters_adults(): void {
-    $users = [
-      ['age' => 20],
-      ['age' => 15],
-    ];
+  public function test_it_filters_adults(array $users, int $expectedCount): void {
+    $this->assertCount($expectedCount, UserFilterAge::filter($users));
+  }
 
-    $this->assertCount(1, UserFilterAge::filter($users));
+  public function usersProvider(): array {
+    return [
+      [
+        [
+          ['age' => 15],
+          ['age' => 20],
+        ],
+        1
+      ],
+      [
+        [
+          ['age' => 18],
+        ],
+        1
+      ]
+    ];
   }
 }
